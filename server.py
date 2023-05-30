0e8a5340d4e51d9d8c2e301cf9d3b8fe5387f515
Steven Borkowski <sborkowski1999@gmail.com>
2023-05-29 18:22:48 -0700

    hello

--- server.py
+++ server.py
@@ -2,18 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
-=======
-@app.route("/")
-def script():
-    return render_template('index.html')
-
-if __name__ == 'main':
-    app.run()
->>>>>>> 3223faba8ae13dfdd1f3b3bf6fe395cc167b2304
