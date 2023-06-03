7b51818ef262b34656b9b03a0af1a2184aa69674
Steven Borkowski <sborkowski1999@gmail.com>
2023-06-03 16:36:13 -0700

    added callback function for map processing

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+import rospy
+from std_msgs.msg import Strings
 
 app = Flask(__name__)
+
+socketio = SocketIO(app)
 
 @app.route('/') 
 def index():
     return render_template('index.html') # check in the template folder
 
+@socketio.on('connect')
+def handle_connect(): # this function is for subscribing to map, and sending data to clients
+    def map_callback(data):
+        #process map data and initialize map_image variable
+
+        #emit map image to Websocket clients
+        socketio.emit('map_update', map_image)
+    rospy.init_node('websocket_node')
+    rospy.Subscriber('/map_topic', String, map_callback) #change map_topic and String
+
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
